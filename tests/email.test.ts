64d9db42ee34c3f96e25b86180843fe80d4a57f9
Giovanni Vignone <giovanni.vignone@duke.edu>
2023-02-05 07:23:30 +0200

    adding test case

--- tests/email.test.ts
+++ tests/email.test.ts
@@ -1,7 +1,11 @@
 import { init, send } from "../src/plugin";
  
-describe('Testing compilation', () => {
-  test('Sample test', () => {
+describe('Testing initialization', () => {
+  test('Launching email manager with configs', () => {
+    if (!process.env.AWS_EMAIL || !process.env.AWS_ACCESS_KEY_ID || !process.env.AWS_SECRET_ACCESS_KEY) {
+      throw new Error('Missing AWS credentials');
+    }
+    const initialize = init('Test Subject', process.env.AWS_EMAIL, process.env.AWS_ACCESS_KEY_ID, process.env.AWS_SECRET_ACCESS_KEY);
     expect(0).toBe(0);
   });
 });
